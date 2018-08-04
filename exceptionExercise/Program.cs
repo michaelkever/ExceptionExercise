ed85cbfa76965b5b31a51911efe18ce87c1f2428
Michael Kever <michaelkever@gmail.com>
2018-08-04 12:19:28 -0500

    Initial commit

---
+++ exceptionExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionExercise
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is your age? \n Press Enter");
+
+                int age = Convert.ToInt32(Console.ReadLine());
+                var today = DateTime.Today.Year;
+                int yearDate = Convert.ToInt32(today);
+                int userBorn = (yearDate - age);
+
+                if (age < 0)
+                {
+                    Console.WriteLine("Answer needs to be a real number, no decimals.");
+                }
+                if (age > 0)
+                {
+                    Console.WriteLine("You were born in {0}", userBorn);
+                }
+                  
+            }
+           catch (Exception)
+            {
+                Console.WriteLine("An error occurred. Please contact System Administator");
+            }
+            
+            Console.ReadLine(); 
+        }
+    }
+}
